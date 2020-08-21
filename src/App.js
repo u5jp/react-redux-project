e4aa8b7f1d476cee7913913685dd7bb55d081368
yugo_i <u15jpn@gmail.com>
2020-08-21 18:29:35 +0900

    いったん

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        hello,world!<br />
-      </p>
+    <div>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}}/>
     </div>
-  );
-  
+  )
 }
+
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "hello worlddd"
+//   );
+// }
+
 
 export default App;
