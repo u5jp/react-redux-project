8f3388d83f14aa4c3a8b995d868ce58a8e58def8
yugo_i <u15jpn@gmail.com>
2020-08-21 19:15:36 +0900

    props of compornent

--- src/App.js
+++ src/App.js
@@ -10,18 +10,28 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako",age:5},
+    {name:"NoName"}
+  ]
   return (
   <div>
-    <Cat/>
-    <Cat/>
-    <Cat/>
-    <Cat/>
+    {
+      profiles.map((profile,index)=>{
+        return <User name = {profile.name} age ={profile.age} key={index}/>
+      })
+    }
   </div>
-    )
+  )
 }
 
-const Cat = () => {
-  return <div>Mewo!</div>
+const User = (props) => {
+  return <div>Hi, Im {props.name} , and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age:1
 }
 
 export default App;
